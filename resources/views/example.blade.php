22acb8f06f10bf1c6adde5fe62e96996c514e295
raoyc <raoyc2009@gmail.com>
2016-09-03 20:14:05 +0800

    update example

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -8,10 +8,10 @@
 <body>
 <h2>wangEditor example</h2>
 
-  <textarea class="form-control we-container" name="content" id="wangeditor" style="display:none;" cols="5">
+    <textarea class="form-control" name="content" id="wangeditor" style="display:none;height:400px;" cols="5">
 <h1>wangEditor for Laravel</h1>
 <p>wangEditor example</p>
-  </textarea>
+    </textarea>
 
 {!! we_js() !!}
 {!! we_config('wangeditor') !!}
