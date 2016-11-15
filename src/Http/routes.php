6d6ba911199266fb1ba10c217c440fc29b2f9847
raoyc <raoyc2009@gmail.com>
2016-11-15 16:14:02 +0800

    fix typo

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,5 +2,4 @@
 
 
 Route::get('laravel-wang-editor/example', 'Douyasi\WangEditor\Http\Controllers\WangEditorController@getExample');
-});
 Route::post('laravel-wang-editor/upload', 'Douyasi\WangEditor\Http\Controllers\WangEditorController@postUploadPicture');
